d6eea539f5070820e7166627f35858919c292db9
Tanguy-Chtln <chatelain.tanguy@lilo.org>
2024-11-26 15:27:55 +0100

    Add backward drive

--- src/PAMI/pathing.c
+++ src/PAMI/pathing.c
@@ -4,12 +4,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <float.h>
 #ifdef ENABLE_DISPLAY
     #include "visual.h"
 #endif
 
 typedef enum { MOTIONLESS = 0, ROTATION, LINEAR, CURVED } MotionState;
-typedef enum { POSITION = 0, SPEED } AsservMode;
+typedef enum { POSITION = 0, SPEED, WAITING } AsservMode;
 
 typedef struct {
     float x;
@@ -22,6 +23,7 @@ typedef struct {
     float distToWPoint;
     float AngularDistToWPoint;
     bool wpChecked;
+    float timeWaited;
     MotionState motionState;
     AsservMode asservMode;
 } State;
@@ -33,6 +35,7 @@ typedef struct {
     float vMax;
     float vaMax;
     float alpha;
+    unsigned int stayStillTime;
     MotionState motionState;
 } Waypoint;
 
@@ -54,8 +57,8 @@ struct PathHandler_t {
     State state;
 };
 
-#define ACC_MAX 0.005f // m/(s^2)
-#define ACC_ANGLE_MAX 0.1f
+#define ACC_MAX 0.5f // m/(s^2)
+#define ACC_ANGLE_MAX 1.6f
 #define SPEED_LIN_MAX 0.1f // m/s
 #define SPEED_DELTA_TRIGGER 0.005f
 #define EPSILON 1e-4f
@@ -85,7 +88,7 @@ float float_min(const float x, const float y) { return x > y ? y : x; }
  */
 
 State State_create(const float x, const float y, const float vLin, const float accLin, const float angle, const float vAngle, const float accAngle,
-                   const float distToWPoint, const float AngularDistToWPoint, MotionState motionState, AsservMode asservMode) {
+                   const float distToWPoint, const float AngularDistToWPoint, const float timeWaited, MotionState motionState, AsservMode asservMode) {
     const State state = {.x = x,
                          .y = y,
                          .vLin = vLin,
@@ -95,14 +98,15 @@ State State_create(const float x, const float y, const float vLin, const float a
                          .accAngle = accAngle,
                          .distToWPoint = distToWPoint,
                          .AngularDistToWPoint = AngularDistToWPoint,
+                         .timeWaited = timeWaited,
                          .motionState = motionState,
                          .asservMode = asservMode,
                          .wpChecked = false};
     return state;
 }
 
-Waypoint Waypoint_create(const float x, const float y, const float vMax, const float vaMax, const float angle, MotionState motionState) {
-    Waypoint wpoint = {.id = UINT_MAX, .x = x, .y = y, .vMax = vMax, .vaMax = vaMax, .alpha = angle, .motionState = motionState};
+Waypoint Waypoint_create(const float x, const float y, const float vMax, const float vaMax, const float angle, const float stayStillTime, MotionState motionState) {
+    Waypoint wpoint = {.id = UINT_MAX, .x = x, .y = y, .vMax = vMax, .vaMax = vaMax, .alpha = angle, .stayStillTime = stayStillTime , .motionState = motionState};
     return wpoint;
 }
 
@@ -182,7 +186,6 @@ void Route_remove(Route *route, unsigned int id) {
     }
 
     const int direction = (route->numNodes / 2) >= id ? 1 : -1;
-    printf("Direction : %d\n", direction);
     WaypointNode *ptr = direction == 1 ? route->start : route->stop;
     unsigned int elmtId = direction == 1 ? 0 : route->numNodes - 1;
     while (ptr != NULL && elmtId != id) {
@@ -192,7 +195,6 @@ void Route_remove(Route *route, unsigned int id) {
             ptr = ptr->previous;
         elmtId += direction;
     }
-    printf("%d %d %p %p\n", elmtId, id, route->start, ptr);
     if (id == 0) {
         route->start = route->start->next;
     }
@@ -230,13 +232,21 @@ float route_get_next_linear_acc(State *state, Route *route, const float dt) {
 
     case LINEAR: {
         const Waypoint *nextWPoint = &route->start->next->wpoint;
-        const float slowDownTrigger =  dt * float_abs(state->vLin - nextWPoint->vMax) * state->vLin / (ACC_MAX * 2);
+        // const float slowDownTrigger =  dt * float_abs(state->vLin - nextWPoint->vMax) * state->vLin / (ACC_MAX *2);
+        // v(t) = ACC * t;
+        // On cherche t tq v(t) = vLin
+        // t = vLin / Acc
+        // d(t) = ACC * t**2 / 2
+        // const float slowDownTrigger =  ( float_abs(state->vLin - nextWPoint->vMax) / (ACC_MAX )) * (float_abs(state->vLin - nextWPoint->vMax) / (ACC_MAX )) * ACC_MAX / 2;
+        const float dv = float_abs(nextWPoint->vMax - state->vLin);
+        const float slowDownTrigger = (dv * dv / (2* ACC_MAX)) + ((nextWPoint->vMax * dv) / ACC_MAX);
+        printf("Trigger : %f\n", slowDownTrigger);
         // Delai point haut et bas :
         if (state->distToWPoint <= slowDownTrigger) {
-            return float_min(float_abs(nextWPoint->vMax - state->vLin), ACC_MAX) * (nextWPoint->vMax - state->vLin >= 0 ? 1 : -1);
+            return float_min(float_abs(nextWPoint->vMax - state->vLin)*dt, ACC_MAX*dt) * (nextWPoint->vMax - state->vLin >= 0 ? 1 : -1);
         }
         const Waypoint *currentWPoint = &route->start->wpoint;
-        return float_min(float_abs(currentWPoint->vMax - state->vLin), ACC_MAX) * (currentWPoint->vMax - state->vLin >= 0 ? 1 : -1);
+        return float_min(float_abs((currentWPoint->vMax - state->vLin)*dt), ACC_MAX*dt) * (currentWPoint->vMax - state->vLin >= 0 ? 1 : -1);
     }
 
     case CURVED:
@@ -260,12 +270,15 @@ float route_get_next_angular_acc(State *state, Route *route, const float dt) {
     case ROTATION: {
         const Waypoint *nextWPoint = &route->start->next->wpoint;
         // const float slowDownTrigger = (state->vAngle - nextWPoint->vaMax) * (state->vAngle - nextWPoint->vaMax) / (2 * ACC_ANGLE_MAX);
-        const float slowDownTrigger = dt * float_abs(state->vAngle - nextWPoint->vaMax) * state->vAngle / (ACC_ANGLE_MAX * 2);
-        if (state->AngularDistToWPoint <= slowDownTrigger) {
-            return float_min(float_abs(nextWPoint->vaMax - state->vAngle), ACC_ANGLE_MAX) * (nextWPoint->vaMax - state->vAngle >= 0 ? 1 : -1);
+        // const float slowDownTrigger = dt * float_abs(state->vAngle - nextWPoint->vaMax) * state->vAngle / (ACC_ANGLE_MAX * 2);
+        const float dv = nextWPoint->vaMax - state->vAngle;
+        const float slowDownTrigger = (dv * dv / (2* ACC_ANGLE_MAX)) + ((nextWPoint->vaMax * dv) / ACC_ANGLE_MAX);
+        printf("Trigger : %f %f\n", slowDownTrigger, state->AngularDistToWPoint);
+        if (float_abs(state->AngularDistToWPoint) <= slowDownTrigger) {
+            return float_min(float_abs(nextWPoint->vaMax - state->vAngle) *dt, ACC_ANGLE_MAX * dt) * (nextWPoint->vaMax - state->vAngle >= 0 ? 1 : -1);
         }
         const Waypoint *currentWPoint = &route->start->wpoint;
-        return float_min(float_abs(currentWPoint->vaMax - state->vAngle), ACC_ANGLE_MAX) * (currentWPoint->vaMax - state->vAngle >= 0 ? 1 : -1);
+        return float_min(float_abs(currentWPoint->vaMax - state->vAngle), ACC_ANGLE_MAX) *dt* (currentWPoint->vaMax - state->vAngle >= 0 ? 1 : -1);
     }
 
     case LINEAR:
@@ -287,19 +300,30 @@ PathHandler pathing_get_handler(const Control_fcts controlFunctions) {
 
     Route route = Route_create();
 
-    const Waypoint wp1 = Waypoint_create(0.1, 1.2, 10., 0, 0, LINEAR);
-    const Waypoint wp2 = Waypoint_create(2.6, 1.2, 0, M_PI / (2), 0, ROTATION);
-    const Waypoint wp3 = Waypoint_create(2.6, 1.2, 0.4, 0, M_PI / 2, LINEAR);
-    const Waypoint wp4 = Waypoint_create(2.6, 0.2, 0, 0, M_PI / 2, MOTIONLESS);
+    // const Waypoint wp1 = Waypoint_create(0.1, 1.2, 1.5, 0, 0, LINEAR);
+    // const Waypoint wp2 = Waypoint_create(2.0, 1.2, 0, M_PI / (2), 0, ROTATION);
+    // const Waypoint wp3 = Waypoint_create(2.0, 1.2, 0.4, 0, M_PI / 2, LINEAR);
+    // const Waypoint wp4 = Waypoint_create(2.0, 0.2, 0, -M_PI, M_PI/2 , ROTATION);
+    // const Waypoint wp5 = Waypoint_create(2.0, 0.2, 1., 0., -M_PI/2, LINEAR);
+    // const Waypoint wp6 = Waypoint_create(2.0, 1.81, 0., 0., -M_PI/2, MOTIONLESS);
+
+
+    const Waypoint wp1 = Waypoint_create(0.1, 1.2, 1.5, 0, 0,0., LINEAR);
+    const Waypoint wp2 = Waypoint_create(2.1, 1.2, 0., 0., 0, 2., MOTIONLESS); 
+    const Waypoint wp3 = Waypoint_create(2.1, 1.2, -1.5, 0., 0., 0.,LINEAR);
+    const Waypoint wp4 = Waypoint_create(1.1, 1.2, 0., 0., 0, FLT_MAX, MOTIONLESS);
+    const Waypoint wp5 = Waypoint_create(1.1, 1.2, 0, 0, 0, FLT_MAX, MOTIONLESS);
 
     Route_add(&route, wp1);
     Route_add(&route, wp2);
     Route_add(&route, wp3);
     Route_add(&route, wp4);
+    Route_add(&route, wp5);
+    // Route_add(&route, wp6);
 
     handler->route = route;
 
-    handler->state = State_create(0.1, 1.2, 0., 0., 0, 0, 0, distance(1.9, 1.2, 0.1, 1.2), 0, LINEAR, SPEED);
+    handler->state = State_create(0.1, 1.2, 0., 0., 0, 0, 0, distance(1.9, 1.2, 0.1, 1.2), 0, 0., LINEAR, SPEED);
 
 #ifdef ENABLE_DISPLAY
     // Visual
@@ -343,30 +367,56 @@ void pathing_mode_speed(struct PathHandler_t *handler, const float dt) {
 void pathing_mode_position(struct PathHandler_t *handler, const float dt) {
     if (handler->controlFunctions.pami_is_position_target_reached()) {
         printf("Adjusted ! Going to next waypoint !\n");
-        handler->state.asservMode = SPEED;
+        if (handler->state.timeWaited > dt) {
+            handler->state.asservMode = WAITING;
+            handler->state.timeWaited = handler->route.start->wpoint.stayStillTime - dt;
+        } else 
+            handler->state.asservMode = SPEED;
+        handler->state.vLin = 0.f;
+        handler->state.vAngle = 0.f;
         Route_remove(&handler->route, 0);
         if (handler->route.start != NULL)
             handler->state.motionState = handler->route.start->wpoint.motionState;
-        else
+        else {
             handler->state.motionState = MOTIONLESS;
+            handler->state.asservMode = WAITING;
+            handler->state.timeWaited = FLT_MAX;
+        }
     }
 }
 
-void pathing_update_speed(const PathHandler _handler, const float dt) {
-    struct PathHandler_t *handler = (struct PathHandler_t *)_handler;
-    if (handler->state.motionState != MOTIONLESS) {
+void pathing_mode_waiting(struct PathHandler_t *handler, const float dt) {
+    if (handler->state.timeWaited <= dt) {
+        handler->state.timeWaited = 0.f;
+        handler->state.asservMode = SPEED;
+    } else {
+        handler->state.timeWaited -= dt;
+    }
+}
+
+void update_handler(struct PathHandler_t *handler) {
         handler->controlFunctions.odometry_func(&handler->state.x, &handler->state.y, &handler->state.angle);
 
         handler->state.distToWPoint =
             distance(handler->state.x, handler->state.y, handler->route.start->next->wpoint.x, handler->route.start->next->wpoint.y);
-        handler->state.AngularDistToWPoint = handler->route.start->next->wpoint.alpha - handler->state.angle;
 
-        if (handler->state.asservMode == SPEED) {
-            pathing_mode_speed(handler, dt);
-        } else if (handler->state.asservMode == POSITION) {
-            pathing_mode_position(handler, dt);
-        }
+        handler->state.AngularDistToWPoint = handler->route.start->next->wpoint.alpha - handler->state.angle;
+}
+
+void pathing_update_speed(const PathHandler _handler, const float dt) {
+    struct PathHandler_t *handler = (struct PathHandler_t *)_handler;
+    if (handler->state.asservMode == SPEED) {
+        update_handler(handler);
+        pathing_mode_speed(handler, dt);
+    }  
+    if (handler->state.asservMode == POSITION) {
+        update_handler(handler);
+        pathing_mode_position(handler, dt);
     }
+    if (handler->state.asservMode == WAITING) {
+        pathing_mode_waiting(handler, dt);            
+    }
+
 
     // Display
 #ifdef ENABLE_DISPLAY
@@ -379,5 +429,5 @@ void pathing_update_speed(const PathHandler _handler, const float dt) {
     displayer_object_update_pos(0, handler->state.x, handler->state.y, handler->state.angle);
 #endif
     printf("x : %f \t  y :%f \t a : %f \t v : %f \t va : %f\n", handler->state.x, handler->state.y, handler->state.angle, handler->state.vLin,
-           handler->state.vAngle);
+            handler->state.vAngle);
 }
